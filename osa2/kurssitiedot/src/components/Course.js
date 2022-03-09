e95dc2e6fc4db22c5cbd30c69193a7298c30e482
Kimmo Surakka <kimmo.surakka@gmail.com>
2022-03-09 11:41:37 +0200

    2.2 kurssitiedot step7

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,10 +1,12 @@
 import Header from "./Header"
 import Content from "./Content"
+import Total from "./Total"
 
 const Course = ({course}) => (
     <div>
       <Header text={course.name} />
       <Content course={course} />
+      <Total course={course} />
     </div>
 )
 
---
+++ osa2/kurssitiedot/src/components/Total.js
@@ -0,0 +1,9 @@
+const Total = ({course}) => (
+  <div><b>
+    Total of {course.parts.reduce((sum, part) =>
+      sum + part.exercises, 0
+    )} exercises
+  </b></div>
+)
+
+export default Total
