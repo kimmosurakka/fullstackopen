5f8f982fdf807ea00351275990514c6e640fdf30
Kimmo Surakka <kimmo.surakka@gmail.com>
2022-03-10 10:50:24 +0200

    2.7 puhelinluettelo step2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -12,9 +12,13 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    if (persons.find(p => p.name === newName) !== undefined) {
+      window.alert(`${newName} is already added to the phonebook`)
+    } else {
+      const newPerson = { name: newName }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
   }
 
   return (
